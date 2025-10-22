b3bbe60fcbaf22fac71698a4d787ea0412234104
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-10-22 15:02:50 +0200

    rework telescope config

--- nvim/lua/opt/telescope.lua
+++ nvim/lua/opt/telescope.lua
@@ -1,19 +1,18 @@
-local telescope = require("telescope")
 local setKeymap = require("core.util").setKeymap
+local telescope = require("telescope")
 
-local extensions = {"conflicts", "dap", "fzf"}
-telescope.setup({
+telescope.setup {
 	extensions = {
 		fzf = {
 			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
 			case_mode = "smart_case",
+			override_file_sorter = true,
+			override_generic_sorter = true,
 		},
 	},
-})
+}
 
-for _, ext in ipairs(extensions) do
+for _, ext in ipairs({"conflicts", "dap", "fzf"}) do
 	telescope.load_extension(ext)
 end
 
@@ -21,11 +20,15 @@ setKeymap({
 	n = {
 		["<leader>fb"] 	= { ":Telescope buffers<CR>", 			desc = "Find buffers" },
 		["<leader>ff"] 	= { ":Telescope find_files<CR>", 		desc = "Find files" },
-		["<leader>fgC"] = { ":Telescope git_commits<CR>", 	desc = "Find git commits" },
-		["<leader>fgb"] = { ":Telescope git_branches<CR>", 	desc = "Find git branches" },
+		["<leader>fF"] 	= {	":Telescope oldfiles<CR>",			desc = "Find old files"},
+		["<leader>fgb"] = { ":Telescope git_branches<CR>",	desc = "Find git branches" },
 		["<leader>fgc"] = { ":Telescope conflicts<CR>", 		desc = "Find git conflicts" },
+		["<leader>fgC"] = { ":Telescope git_commits<CR>", 	desc = "Find git commits" },
+		["<leader>fgS"] = { ":Telescope git_stash<CR>", 		desc = "Show git stash" },
 		["<leader>fgs"] = { ":Telescope git_status<CR>", 		desc = "Show git status" },
+		["<leader>fk"]	= {	":Telescope keymaps<CR>", 			desc = "Find keymaps" },
 		["<leader>fld"] = { ":Telescope diagnostics<CR>", 	desc = "Find diagnostics" },
+		["<leader>fm"] 	= {	":Telescope marks<CR>", 				desc = "Find marks" },
 		["<leader>fn"] 	= { ":Telescope notify<CR>", 				desc = "Find notifications" },
 		["<leader>fs"] 	= { ":Telescope live_grep<CR>", 		desc = "Live grep" },
 	}
